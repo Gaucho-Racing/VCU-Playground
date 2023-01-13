400611d9dea5c48dfeb29a99f1d3d49020399352
Rick T. Zhang <rtzhang@ucsb.edu>
2023-01-13 03:55:59 +0000

    Prepatory stuff for actual motor logic states

--- main.cpp
+++ main.cpp
@@ -12,3 +12,12 @@ motor
 brake
 lights (brake lights, anyone?)
 */
+
+#include <iostream>;
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+   cout << "AAAAAAAAAAAAAAAAA!" << endl;
+   return 0;
+}
+
