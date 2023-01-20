635682229fa4637c4577bebdaa31fd6978b67e36
Rick T. Zhang <rtzhang@ucsb.edu>
2023-01-20 03:41:26 +0000

    small update to motor class

--- src/motor/motor.cpp
+++ src/motor/motor.cpp
@@ -1,10 +1,21 @@
 #include "motor.h"
 using namespace std;
 
-int Motor::main() {
-   return 0; //stub, ctrl+c, ctrl+v main function from lightbulb tests and modify  
+//@willam, please help program the logic of the motor main function
+int Motor::state_update() {
+   cout << "MOTOOOOOOOOOOOOOOOOOOOOOOOOOOR!" << endl;
+   //update the inputs to the motor of the motor class (start_pressed, motor_speed, throttle_pod, brake_pod)
+   //return control to main, if necessary (off or fail)
+   //run one instance of the while loop of the lightbulb tests, only modified for motor logic.
+   //set next state (private static string variable state) to next state as returned by the state function that was run above. 
+   return 0; 
 }
 
 #include "throttle.cpp"
 //#include "brake.cpp"
 //#include "idle.cpp"
+
+
+//main function runs a loop that consistantly runs the "state_update" functions of each macro-state class
+//"state_update" runs a private function of corresponding state class and sets the next state of this macro-state class
+//each state function that executes one loop of the current state
--- src/motor/motor.h
+++ src/motor/motor.h
@@ -6,16 +6,28 @@ using namespace std;
 
 class Motor {
    public:
-      int main();
+      /* 
+      * The main running function of the motor state machine. 
+      * This will control the states the motor goes through based on the outputs of the previous state. 
+      * -rt.z
+      *
+      * @returns int return type is a stub. planning to return something to notify core main function whether to shutdown the car completely (off switch, something failed), move to next step in processing logic, or some other special case.
+      */
+      int state_update();
+   private:
       string throttle();
       string brake();
       string idle();
-   private:
-      static int const zero = 0; //[placeholder]
+      
+      static string state; //the current state of the motor logic
       static bool start_pressed; 
       static int motor_speed; // [0, 100]
-      static int throttle_pos; // [0, 100]
-      static int brake_pos; // [0, 1]
-}
+      static int throttle_pod; // [0, 100]
+      static int brake_pod; // [0, 1]
+
+      static int set_motor_speed(int speed); // in: [0, 100], out:[0, 100] (current speed)
+
+      //other data to be stored.
+};
 
 #endif
