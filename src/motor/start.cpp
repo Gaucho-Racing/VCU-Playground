69e508099725ecf08393faa725d69054cbbe64ba
Rick T. Zhang <rtzhang@ucsb.edu>
2023-01-27 04:39:00 +0000

    Motor.start, pseudocode

--- src/motor/start.cpp
+++ src/motor/start.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 
 string Motor::ST_F_start() {
-   //starting procedure
+   //starting procedure, if any
+   cout << "Motor is starting" << endl;
    Motor::set_motor_speed(Motor::throttle_pod);
+   //check output of motor??? 
    //InternalEvent to throttle
 }
