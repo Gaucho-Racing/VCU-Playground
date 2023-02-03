a439301fc2258c56d5dc2c3241fc626befe712e8
Yarwin Liu <yarwinliu@ucsb.edu>
2023-02-02 20:40:41 -0800

    do not charge file

---
+++ src/battery/do_not_charge.cpp
@@ -0,0 +1,10 @@
+// if there is any error, then don't charge
+
+//already checked not moving, no errors (voltage)
+bool chargeOrNot () {
+    if () {  //if something is wrong, abort
+        return false;
+    } else {  //change state to charging
+        return true;
+    }
+}
