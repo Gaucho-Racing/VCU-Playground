86e8f7c35d4a162ab3fb7e76398f404dde8a2516
Rick T. Zhang <rtzhang@ucsb.edu>
2023-02-03 03:14:56 +0000

    small update to start.cpp

--- src/motor/start.cpp
+++ src/motor/start.cpp
@@ -8,7 +8,7 @@ using namespace std;
 string Motor::ST_F_start() {
    //starting procedure, if any
    cout << "Motor is starting" << endl;
-   Motor::set_motor_speed(Motor::throttle_pod);
+   Motor::setCurrent(Motor::throttle_pod);
    //check output of motor??? 
    //InternalEvent to throttle
 }
