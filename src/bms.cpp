5f7a2150d9ff650cd1f45c730373f26eb7552163
Rick T. Zhang <rtzhang@ucsb.edu>
2023-02-19 23:16:58 +0000

    basic bms logic done

--- src/bms.cpp
+++ src/bms.cpp
@@ -8,7 +8,7 @@ int state_0_ready_to_charge() {
    cout << "CAN: Command BMS/AIR to be ready to accept positive current..." << endl; //in the case do_not_charge sent the opposite command earlier.
    cout << "CAN: Dashboard stuff. Indication may not be necessary." << endl;
    while (1) {
-      if (1 /*fail conditions met*/) {
+      if (0 /*fail conditions met*/) {
          return 19;
       }
       //continously execute state tasks
@@ -28,7 +28,7 @@ int state_1_not_charging() { //an extended version of precharge that includes th
    cout << "VCU: Switching state to 1: not charging…" << endl;
    cout << "CAN: Send signal to dashboard to indicate NOT CHARGING." << endl;
    while (1) {
-      if (1 /*fail conditions met*/) {
+      if (0 /*fail conditions met*/) {
          return 19;
       }
       //continously execute state tasks
@@ -41,3 +41,40 @@ int state_1_not_charging() { //an extended version of precharge that includes th
       }
    }
 } 
+
+int state_2_charging() {
+   cout << "VCU: Switching state to 2: charging..." << endl;
+   cout << "CAN: Send signal to dashboard to indicate CHARGING." << endl;
+   while (1) {
+      if (0 /*fail conditions met*/) {
+         return 19;
+      }
+      //continously execute state tasks
+      cout << "VCU/CAN: Do necessary things here" << endl;
+      //check for state change conditions
+      if (getChargeState() >= 99) {
+         return 3;
+      } else if (getCurrent() < -10) {
+         return 0;
+      } else if (getCurrent() <= 10) {
+         return 1;
+      } e
+   }
+}
+
+int state_3_charging() {
+   cout << "VCU: Switching state to 3: fully charged..." << endl;
+   cout << "CAN: Send signal to dashboard to indicate FULLY CHARGED." << endl;
+   while (1) {
+      if (0 /*fail conditions met*/) {
+         return 19;
+      }
+      //continously execute state tasks
+      cout << "VCU/CAN: Do necessary things here" << endl;
+      if (getChargeState() < 99 && getCurrent() >= -10) {
+         return 2;
+      } else if (getCurrent() < -10) {
+         return 0;
+      }
+   }
+}
